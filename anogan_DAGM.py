447cab59af86f26b97f93aed31e64d9739113dea
tabularasa066a <tabularasa066a@gmail.com>
2020-09-22 21:35:43 +0900

    reve

--- anogan_DAGM.py
+++ anogan_DAGM.py
@@ -11,6 +11,8 @@ import os
 import shutil
 from pathlib import Path
 
+IMAGE_CHANNELS = 3
+
 IMAGE_SIZE = 128  # mnistの画像サイズが28 x 28なのでとりあえず
 IMAGE_SIZE_HALF = int(IMAGE_SIZE / 2)
 IMAGE_SIZE_QUARTED = int(IMAGE_SIZE / 4)
@@ -134,7 +136,7 @@ class DCGAN(object):
                 progress_bar.update(index, values=[('g', g_loss), ('d', d_loss)])
             g_losses.append(g_loss)
             d_losses.append(d_loss)
-            
+
             ## 生成画像の保存
             if (epoch+1)%10 == 0:
                 image = self.combine_images(generated_images)
@@ -212,38 +214,40 @@ def load_imgs(root_dir, class_name):
         if 'png' in img_name:  # .DL_shareやら*.txtが存在するときそれらをスキップ
             img_paths.append(os.path.abspath(os.path.join(root_dir, class_name, img_name)))
     for img_path in img_paths:
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)  # グレースケール読み込み
+        # img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)  # グレースケール読み込み
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, dsize=(IMAGE_SIZE, IMAGE_SIZE))
         images.append(img)
     images = np.array(images)
-    images.reshape(images.shape[0], IMAGE_SIZE, IMAGE_SIZE, 1)
-    
+    images.reshape(images.shape[0], IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
+
     return images
 
 # train
 if __name__ == '__main__':
     batch_size = 16
-    epochs = 100   
-    # epochs = 10000   
-    input_dim = 300
+    # epochs = 100
+    epochs = 1
+    input_dim = 100
     g_optim = Adam(lr=0.0001, beta_1=0.5, beta_2=0.999)
     d_optim = Adam(lr=0.0001, beta_1=0.5, beta_2=0.999)
     ## DAGMファイル読み取る用改修
     img_data_Normal = load_imgs(str(Path('DAGM2007/Normal').resolve()), 'Class1')
     img_data_Normal = img_data_Normal.astype(np.float32) / 255
-    img_data_Normal = img_data_Normal.reshape(img_data_Normal.shape[0], IMAGE_SIZE, IMAGE_SIZE, 1)  # (1000, 28, 28, 1)
+    img_data_Normal = img_data_Normal.reshape(img_data_Normal.shape[0], IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)  # (1000, 28, 28, 1)
     img_data_withDefects = load_imgs(str(Path('DAGM2007/withDefects').resolve()), 'Class1_def')
     img_data_withDefects = img_data_withDefects.astype(np.float32) / 255
-    img_data_withDefects = img_data_withDefects.reshape(img_data_withDefects.shape[0], IMAGE_SIZE, IMAGE_SIZE, 1)  # (150, 28, 28, 1)
+    img_data_withDefects = img_data_withDefects.reshape(img_data_withDefects.shape[0], IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)  # (150, 28, 28, 1)
     # # 訓練用画像枚数を画像総数の8割とし、テスト画像枚数を残りの２割として抽出
-    # train_image_num = int(img_data_Normal.shape[0] * 0.8) 
+    # train_image_num = int(img_data_Normal.shape[0] * 0.8)
     # x_train = img_data_Normal[0:train_image_num]
     # x_test = img_data_Normal[train_image_num:]
 
     # x_train_1 = img_data_Normal
     # x_test_9 = img_data_withDefects
 
-    # TODO: 訓練用画像(正常系のみ): 850枚, テスト用画像(正常系+異常系): 150 + 150 = 300枚 
+    # TODO: 訓練用画像(正常系のみ): 850枚, テスト用画像(正常系+異常系): 150 + 150 = 300枚
     normal_imgs4train = 850
     x_train_1 = img_data_Normal[0:normal_imgs4train]  # TODO: x_train_1には正常系の画像のみ入れること
 
@@ -263,36 +267,37 @@ if __name__ == '__main__':
     ## 学習データの作成
     # x_train_1 = []  # 正常系の画像のみ学習のため、1とラベル付けされているものみ抽出
     # for i in range(len(x_train)):
-    #     if y_train[i] == 1: 
+    #     if y_train[i] == 1:
     #     #    print("x_train[i]", x_train[i].shape)
-    #        x_train_1.append(x_train[i].reshape((28, 28, 1))) 
+    #        x_train_1.append(x_train[i].reshape((28, 28, 1)))
     #     #    print("x_train_1", np.array(x_train_1).shape)
     # x_train_1 = np.array(x_train_1)
     print("x_train_1", x_train_1.shape)
     print("train data:",len(x_train_1))
-  
+
     ## 評価データの作成
     cnt = 0
     x_test_9, y = [], []
     for i in range(len(x_test)):
-        if y_test[i] == 1 or y_test[i] == 9:  
-           x_test_9.append(x_test[i].reshape((IMAGE_SIZE, IMAGE_SIZE, 1))) 
+        if y_test[i] == 1 or y_test[i] == 9:
+           x_test_9.append(x_test[i].reshape((IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)))
            y.append(y_test[i])
            cnt +=1
         #    if cnt == 100:
-        #       break    
+        #       break
            if cnt == 300:
-              break        
+              break
     x_test_9 = np.array(x_test_9)
-    print("test_data:",len(x_test_9))    
-    input_shape = x_train_1[0].shape 
-    X_test_original = x_test_9.copy()    
+    print("test_data:",len(x_test_9))
+    input_shape = x_train_1[0].shape
+    print(input_shape)
+    X_test_original = x_test_9.copy()
 
-   
+
     # train generator & discriminator
     dcgan = DCGAN(input_dim, input_shape)
     dcgan.compile(g_optim, d_optim)
-    g_losses, d_losses = dcgan.train(epochs, batch_size, x_train_1)  
+    g_losses, d_losses = dcgan.train(epochs, batch_size, x_train_1)
     with open('loss.csv', 'w') as f:
         for g_loss, d_loss in zip(g_losses, d_losses):
             f.write(str(g_loss) + ',' + str(d_loss) + '\n')
@@ -302,7 +307,7 @@ K.set_learning_phase(1)
 
 def denormalize(X):
     return ((X + 1.0)/2.0*255.0).astype(dtype=np.uint8)
-            
+
 if __name__ == '__main__':
     iterations = 100
     # iterations = 10000
@@ -310,12 +315,12 @@ if __name__ == '__main__':
     input_dim = 100
 
     anogan_optim = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
-
+    print(input_shape)
     # load weights
     dcgan = DCGAN(input_dim, input_shape)
     dcgan.load_weights('weights/generator_99.h5', 'weights/discriminator_99.h5')  #3999.99
 
-    for i, test_img in enumerate(x_test_9):  
+    for i, test_img in enumerate(x_test_9):
         test_img = test_img[np.newaxis,:,:,:]
         anogan = ANOGAN(input_dim, dcgan.g)
         anogan.compile(anogan_optim)
@@ -326,17 +331,17 @@ if __name__ == '__main__':
         cv2.imwrite('predict' + os.sep + str(int(anomaly_score)) + '_' + str(i) + '.png', imgs)
         print(str(i) + ' %.2f'%anomaly_score)
 
-        if y[i] == 1 :       
+        if y[i] == 1 :
            with open('scores_Normal.txt', 'a') as f:
                 f.write(str(anomaly_score) + '\n')
         else:
            with open('scores_Defects.txt', 'a') as f:
-                f.write(str(anomaly_score) + '\n') 
-    
+                f.write(str(anomaly_score) + '\n')
+
     # plot histgram
     import matplotlib.pyplot as plt
     import csv
-    
+
     x =[]
     with open('scores_Normal.txt', 'r') as f:
          reader = csv.reader(f)
@@ -349,7 +354,7 @@ if __name__ == '__main__':
          for row in reader:
              row = int(float(row[0]))
              y.append(row)
-             
+
     plt.title("Histgram of Score")
     plt.xlabel("Score")
     plt.ylabel("freq")
